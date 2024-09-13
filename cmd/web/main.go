329ddabe9331e0408ce5e7cb397996d260b18bb5
Firdavs <firdavs20fe@gmail.com>
2024-09-13 21:28:54 +0300

    feat: add config file and load env variables

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/sail-host/cloud/config"
 )
 
 func main() {
+	config.LoadConfig()
 	fmt.Println("Hello, World!")
+	fmt.Println(config.GetConfig())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/sail-host/cloud
 
 go 1.22.1
+
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	github.com/joho/godotenv v1.5.1
+)
