f7af5427d35199a40f89a23f1da5021b72d1ea28
abek01sulaymonov <abek01sulaymonov@gmail.com>
2024-11-01 02:57:12 +0500

    Feat: add rate limiter middleware to control API request rates
    - Implemented RateLimiter middleware to limit the number of requests per client IP within a specified duration
    - Added in-memory rate limiter using "golang.org/x/time/rate" package
    - Ensured thread-safe access to rate limiter map using a mutex
    - Responds with 429 status code when request limit is exceeded

--- internal/init/router/router.go
+++ internal/init/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"log"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
@@ -31,6 +32,9 @@ func Routers() *echo.Echo {
 	// TODO: Add this middleware for log and debug
 	Router.Use(echoMiddleware.Logger())
 	Router.Use(echoMiddleware.Recover())
+
+	// rate limiter middleware
+	Router.Use(middleware.RateLimiter(5, 30*time.Second)) // 5 requests per 30 seconds
 
 	setStatic(Router)
 
